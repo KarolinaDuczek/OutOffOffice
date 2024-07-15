5eb91fc3b9f750dc50db04b9e839028b14063bfd
Karolina <karlazar11@gmail.com>
2024-07-15 21:25:48 +0200

    updates

--- OutOfOffice/OutOfOffice_web/Models/ApprovalRequest.cs
+++ OutOfOffice/OutOfOffice_web/Models/ApprovalRequest.cs
@@ -1,12 +1,18 @@
-﻿namespace OutOfOffice_web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OutOfOffice_web.Models
 {
+
+    [Display(Name = "Approval Request")]
     public class ApprovalRequest
     {
         public int Id { get; set; }
+        [Display(Name ="Approver")]
         public int ApproverId { get; set; }
         public virtual Employee? Approver {get; set;}
+        [Display(Name = "Leave Request Id")]
         public int LeaveRequestId { get; set; }
-        public Selection.Status Status { get; set; }
+        public Selection.RequestStatus Status { get; set; }
         public string? Comment { get; set; }
         public virtual LeaveRequest? LeaveRequest { get; set; }
     }
--- OutOfOffice/OutOfOffice_web/Models/Project.cs
+++ OutOfOffice/OutOfOffice_web/Models/Project.cs
@@ -1,11 +1,19 @@
-﻿namespace OutOfOffice_web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OutOfOffice_web.Models
 {
     public class Project
     {
         public int Id { get; set; }
+        [Display(Name ="Project Type")]
         public Selection.ProjectType ProjectType { get; set; }
+        [Display(Name = "Start Date")]
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
+        [Display(Name = "End Date")]
+        [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
+        [Display(Name = "Project Manager")]
         public int ProjectManagerId { get; set; }
         public virtual Employee? ProjectManager { get; set; }
         public string? Comment { get; set; }
